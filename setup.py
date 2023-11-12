7d86d6a3126f5b44ebb0bab697482bb32ec8c2fd
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-11-12 01:37:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
-
-setup(
-    name='flask-turnkey',
-    version='0.0.1',
-    packages=['flask_turnkey.flask_rum', 'flask_turnkey.flask_turboduck', 'flask_turnkey.flask_turboduck.tests'],
-    url='http://github.com/dommerttech/flask-turnkey',
-    license='mit',
-    author='Dommert',
-    author_email='Dommert@Gmail.com',
-    description='Provides starting point for flask projects. includes themes, templates,Database ORM, admin, user system, rest api.'
-)
+from 
+distutils.core
+import setup
+setup(name =_.flask-turnkey_
+version= 0.1
+packages
+['flask_turnkey.flask_rum
+flask_turnkey.flask_turboduck'
+flask_turnkey.flask_turboduck.tests']    url='http://github.com/dommerttech/ flask-turnkey, 
+license = mit,
+author = Dommert,    author_email='Dommert@Gmail.com',
+description='Provides starting point for flask projects. 
+includes themes,templates,Database ORM, admin, user system, rest api.')
